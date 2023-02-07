cbbe43d7afb6da8ffb70238461c563778ff68041
Unir reis <heizzdesign@gmail.com>
2023-02-07 19:36:19 -0300

    Atualized Movement

--- Project_Grunts/Assets/Project/Script/Player_Move.cs
+++ Project_Grunts/Assets/Project/Script/Player_Move.cs
@@ -43,14 +43,15 @@ public class Player_Move : MonoBehaviour
     #region Habilities
 
     //Dash
-    public bool isHolding;
+    public bool isHolding = false;
     public float timetoPress;
     public float timetoPressBase;
     public int DashForce;
     public int[] keyPressCounts = new int[8];
     private float cooldownTimer;
     public float cooldownDuration = 1f;
-    
+    public KeyCode lastKey = KeyCode.None;
+    public KeyCode currentKey = KeyCode.None;
     // Jump
 
     //Basic Atak
@@ -131,37 +132,102 @@ public class Player_Move : MonoBehaviour
         CheckDoublePress(KeyCode.S, 6);
         CheckDoublePress(KeyCode.D, 7);
 
+        
+
     }
 
 
-    void CheckDoublePress(KeyCode key, int index)
+   void CheckDoublePress(KeyCode key, int index)
+{
+    if (Input.GetKeyDown(key))
     {
-        if(!isHolding)
+
+        if (!isHolding) 
         {
-            if (Input.GetKeyDown(key))
+
+                if (key == lastKey)
+                {
+                    keyPressCounts[index]++;
+                    if (keyPressCounts[index] == 2)
+                    {
+                        if (Time.time >= cooldownTimer)
+                        {
+                            Debug.Log(key.ToString() + " pressed twice!");
+                            rb.AddForce(transform.forward * DashForce, ForceMode.Impulse);
+                            cooldownTimer = Time.time + cooldownDuration;
+                        }
+                        for (int i = 0; i < keyPressCounts.Length; i++)
+                        {
+                            keyPressCounts[i] = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < keyPressCounts.Length; i++)
+                    {
+                        keyPressCounts[i] = 0;
+                    }
+                    keyPressCounts[index] = 1;
+                }
+
+           
+        }
+        else
+        {
+                if (key == lastKey)
+                {
+                    keyPressCounts[index]++;
+                    if (keyPressCounts[index] == 3)
+                    {
+                        if (Time.time >= cooldownTimer)
+                        {
+                            Debug.Log(key.ToString() + " pressed twice!");
+                            rb.AddForce(transform.forward * DashForce, ForceMode.Impulse);
+                            cooldownTimer = Time.time + cooldownDuration;
+                        }
+                        for (int i = 0; i < keyPressCounts.Length; i++)
+                        {
+                            keyPressCounts[i] = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < keyPressCounts.Length; i++)
+                    {
+                        keyPressCounts[i] = 0;
+                    }
+                    keyPressCounts[index] = 1;
+                }
+            
+        }
+            lastKey = key;
+        }
+}
+
+    void CheckKeyPress(KeyCode key, int index)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            if (key == lastKey)
             {
                 keyPressCounts[index]++;
-                if (keyPressCounts[index] == 2)
-                {
-                    if (Time.time >= cooldownTimer)
-                    {
-                        Debug.Log(key.ToString() + " pressed twice!");
-                        rb.AddForce(transform.forward * 100, ForceMode.Impulse);
-                        cooldownTimer = Time.time + cooldownDuration;
-                    }
-                    keyPressCounts[index] = 0;
-                }
             }
+            else
+            {
+                for (int i = 0; i < keyPressCounts.Length; i++)
+                {
+                    keyPressCounts[i] = 0;
+                }
+                keyPressCounts[index] = 1;
+            }
+            lastKey = key;
+        }
 
-        }
-       
-       
-        if(isHolding) 
-        {
-            keyPressCounts[index] = 0;
-        }
 
     }
+
     public void VerifyDoubleClick()
     {
 
