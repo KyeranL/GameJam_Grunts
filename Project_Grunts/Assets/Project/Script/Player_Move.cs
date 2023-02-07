dbbee5bf424bf2d503f6afddad1747579bbecf1f
Unir reis <heizzdesign@gmail.com>
2023-02-06 23:55:12 -0300

    Update codes

--- Project_Grunts/Assets/Project/Script/Player_Move.cs
+++ Project_Grunts/Assets/Project/Script/Player_Move.cs
@@ -1,11 +1,17 @@
+using JetBrains.Annotations;
+using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player_Move : MonoBehaviour
 {
+    #region Movement Variables
+    //8 DIRECTION MOVEMENT
     float vertical;
     float horizontal;
 
@@ -14,19 +20,66 @@ public class Player_Move : MonoBehaviour
 
     Vector3 targetRotation;
 
-    float rotationSpeed = 10;
-    float speed = 200f;
+    [Header("Player Status")]
 
-    Rigidbody rb;       
+    //Movement
+    public float speed = 200f;
+    public float rotationSpeed = 10;
+
+
+    //Life//
+    int Life;
+    int Atk;
+    #endregion
+
+    #region Player Components
+
+    Rigidbody rb;
+
+    #endregion
+
+    #region Habilities
+
+    //Dash
+    public bool isHolding;
+    public float timetoPress;
+    public float timetoPressBase;
+    public int DashForce;
+    public int[] keyPressCounts = new int[8];
+    private float[] cooldownTimers = new float[8];
+    public float cooldownDuration = 1f;
+    // Jump
+
+    //Basic Atak
+
+    //Shoot
+
+    //Blast
+
+    #endregion
+
     void Start()
     {
+
         rb = GetComponent<Rigidbody>();
+       
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Movement();
+
+
+
+
+    }
+
+    public void Update()
+    {
+        VerifyDoubleClick();
+
+        CheckKey();
     }
 
     public void Movement()
@@ -45,19 +98,106 @@ public class Player_Move : MonoBehaviour
             input.Normalize();
         }
 
-        if(inputRaw.sqrMagnitude > 1f)
+        if (inputRaw.sqrMagnitude > 1f)
         {
             inputRaw.Normalize();
         }
 
-        if(inputRaw != Vector3.zero)
+        if (inputRaw != Vector3.zero)
         {
             targetRotation = Quaternion.LookRotation(input).eulerAngles;
         }
 
         rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetRotation.x, Mathf.Round(targetRotation.y / 45) * 45, targetRotation.z), Time.deltaTime * rotationSpeed);
         //Movement
+
         Vector3 vel = input * speed * Time.deltaTime;
         rb.velocity = vel;
+
     }
+
+    public void CheckKey()
+    {
+        CheckDoublePress(KeyCode.UpArrow, 0);
+        CheckDoublePress(KeyCode.DownArrow, 1);
+        CheckDoublePress(KeyCode.LeftArrow, 2);
+        CheckDoublePress(KeyCode.RightArrow, 3);
+        CheckDoublePress(KeyCode.W, 4);
+        CheckDoublePress(KeyCode.A, 5);
+        CheckDoublePress(KeyCode.S, 6);
+        CheckDoublePress(KeyCode.D, 7);
+
+    }
+
+
+    void CheckDoublePress(KeyCode key, int index)
+    {
+        if(!isHolding)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                keyPressCounts[index]++;
+                if (keyPressCounts[index] == 2)
+                {
+                    if (Time.time >= cooldownTimers[index])
+                    {
+                        Debug.Log(key.ToString() + " pressed twice!");
+                        rb.AddForce(transform.forward * DashForce, ForceMode.Impulse);
+                        cooldownTimers[index] = Time.time + cooldownDuration;
+                    }
+                    keyPressCounts[index] = 0;
+                }
+            }
+        }
+       
+       
+        if(isHolding) 
+        {
+            keyPressCounts[index] = 0;
+        }
+
+    }
+    public void VerifyDoubleClick()
+    {
+
+
+        if (horizontal > 0 || vertical > 0 || horizontal < 0 || vertical < 0)
+        {
+            timetoPress -= Time.deltaTime;
+            if (timetoPress < timetoPressBase / 2)
+            {
+                isHolding = true;
+            }
+            else
+            {
+                isHolding = false;
+
+            }
+        }
+        else
+        {
+            isHolding = false;
+            timetoPress = timetoPressBase;
+
+        }
+        if (timetoPress < 0)
+        {
+            timetoPress = 0;
+        }
+        
+
+    }
+
+   
 }
+
+
+   
+
+
+
+
+
+
+
+
